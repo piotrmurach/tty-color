823c0734bc34d94fe87b0629dbb934025daf2c61
Piotr Murach <pmurach@gmail.com>
2016-01-02 21:46:14 +0100

    Change to detect more terminal types.

--- lib/tty/color/mode.rb
+++ lib/tty/color/mode.rb
@@ -3,6 +3,28 @@
 module TTY
   module Color
     class Mode
+      TERM_256 = /iTerm.app/x
+
+      TERM_64 = /^(hpterm-color|wy370|wy370-105k|wy370-EPC|wy370-nk|
+                 wy370-rv|wy370-tek|wy370-vb|wy370-w|wy370-wvb)$/x
+
+      TERM_52 = /^(dg+ccc|dgunix+ccc|d430.*?[\-\+](dg|unix).*?[\-\+]ccc)$/x
+
+      TERM_16 = /^(amiga-vnc|d430-dg|d430-unix|d430-unix-25|d430-unix-s|
+                 d430-unix-sr|d430-unix-w|d430c-dg|d430c-unix|d430c-unix-25|
+                 d430c-unix-s|d430c-unix-sr|d430c-unix-w|d470|d470-7b|d470-dg|
+                 d470c|d470c-7b|d470c-dg|dg+color|dg\+fixed|dgunix\+fixed|
+                 dgmode\+color|hp\+color|ncr260wy325pp|ncr260wy325wpp|
+                 ncr260wy350pp|ncr260wy350wpp|nsterm|nsterm-c|nsterm-c-acs|
+                 nsterm-c-s|nsterm-c-s-7|nsterm-c-s-acs|nsterm\+c|
+                 nsterm-7-c|nsterm-bce)$/x
+
+      TERM_8 = /vt100|xnuppc|wy350/x
+
+      def initialize(env)
+        @env = env
+      end
+
       # Detect supported colors
       #
       # @return [Integer]
@@ -26,13 +48,14 @@ module TTY
       #
       # @api private
       def from_term
-        case TTY::Color.term
+        case @env['TERM']
         when /[\-\+](\d+)color/ then $1.to_i
         when /[\-\+](\d+)bit/   then 2 ** $1.to_i
-        when /wy370-|hpterm-color/ then 64
-        when /d430.*?[\-\+](dg|unix).*?[\-\+]ccc/ then 52
-        when /d430.*?[^c]{3}/ then 16
-        when /vt100/ then 8
+        when TERM_256 then 256
+        when TERM_64  then 64
+        when TERM_52  then 52
+        when TERM_16  then 16
+        when TERM_8   then 8
         when /dummy/ then 0
         else NoValue
         end
