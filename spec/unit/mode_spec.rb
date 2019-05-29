b487d63aa5a0f1dd52baa918109311be684ff5be
Piotr Murach <pmurach@gmail.com>
2019-05-29 20:03:57 +0100

    Change to freeze strings

--- spec/unit/mode_spec.rb
+++ spec/unit/mode_spec.rb
@@ -1,4 +1,4 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
 RSpec.describe TTY::Color::Mode, 'detecting mode' do
   it "isn't terminal" do
--- spec/unit/supports_spec.rb
+++ spec/unit/supports_spec.rb
@@ -1,4 +1,4 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
 RSpec.describe TTY::Color::Support, '#supports?' do
   it "doesn't check color support for non tty terminal" do
