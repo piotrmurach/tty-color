d860cbdcf3bff619f91ae185544e138dfe221255
Piotr Murach <pmurach@gmail.com>
2020-11-07 12:45:59 +0100

    Change mode to freeze and remove redundant escapes from regexes

--- lib/tty/color/mode.rb
+++ lib/tty/color/mode.rb
@@ -3,17 +3,17 @@
 module TTY
   module Color
     class Mode
-      TERM_24BIT = /[+-]direct/
-      TRUECOLORS = 2 ** 24 # 8 bits per RGB channel
+      TERM_24BIT = /[+-]direct/.freeze
+      TRUECOLORS = 2**24 # 8 bits per RGB channel
 
       TERM_256 = /^(alacritty|iTerm\s?\d*\.app|kitty|mintty|ms-terminal|
                     nsterm|nsterm-build\d+|terminator|terminology(-[0-9.]+)?|
-                    termite|vscode)$/x
+                    termite|vscode)$/x.freeze
 
       TERM_64 = /^(hpterm-color|wy370|wy370-105k|wy370-EPC|wy370-nk|
-                 wy370-rv|wy370-tek|wy370-vb|wy370-w|wy370-wvb)$/x
+                   wy370-rv|wy370-tek|wy370-vb|wy370-w|wy370-wvb)$/x.freeze
 
-      TERM_52 = /^(dg+ccc|dgunix+ccc|d430.*?[\-\+](dg|unix).*?[\-\+]ccc)$/x
+      TERM_52 = /^(dg+ccc|dgunix+ccc|d430.*?[-+](dg|unix).*?[-+]ccc)$/x.freeze
 
       TERM_16 = /^(amiga-vnc|d430-dg|d430-unix|d430-unix-25|d430-unix-s|
                  d430-unix-sr|d430-unix-w|d430c-dg|d430c-unix|d430c-unix-25|
@@ -22,9 +22,9 @@ module TTY
                  dgmode\+color|hp\+color|ncr260wy325pp|ncr260wy325wpp|
                  ncr260wy350pp|ncr260wy350wpp|nsterm-c|nsterm-c-acs|
                  nsterm-c-s|nsterm-c-s-7|nsterm-c-s-acs|nsterm\+c|
-                 nsterm-7-c|nsterm-bce)$/x
+                 nsterm-7-c|nsterm-bce)$/x.freeze
 
-      TERM_8 = /vt100|xnuppc|wy350/x
+      TERM_8 = /vt100|xnuppc|wy350/x.freeze
 
       METHODS = %w[from_term from_tput].freeze
 
@@ -57,8 +57,8 @@ module TTY
       def from_term
         case @env["TERM"]
         when TERM_24BIT then TRUECOLORS
-        when /[\-\+](\d+)color/ then $1.to_i
-        when /[\-\+](\d+)bit/   then 2 ** $1.to_i
+        when /[-+](\d+)color/ then $1.to_i
+        when /[-+](\d+)bit/   then 2**$1.to_i
         when TERM_256 then 256
         when TERM_64  then 64
         when TERM_52  then 52
@@ -78,6 +78,7 @@ module TTY
         if !TTY::Color.command?("tput colors")
           return NoValue
         end
+
         colors = `tput colors 2>/dev/null`.to_i
         colors >= 8 ? colors : NoValue
       rescue Errno::ENOENT
